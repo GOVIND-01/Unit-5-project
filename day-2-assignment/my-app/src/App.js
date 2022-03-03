2673d28b89f1f9d235327dd11197ad3d26127d46
Govind Singh <grvrautela90@gmail.com>
2022-03-04 00:00:51 +0530

    Assignment

---
+++ day-2-assignment/my-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Mobile Oprerating System</h1>
+      <ul>
+        <li>Android</li>
+        <li>Blakberry</li>
+        <li>iPhone</li>
+        <li>Windows Phone</li>
+      </ul>
+      <h1>Mobile Manufactures</h1>
+      <ul>
+        <li>Samsung</li>
+        <li>HTC</li>
+        <li>Micromax</li>
+        <li>Apple</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
