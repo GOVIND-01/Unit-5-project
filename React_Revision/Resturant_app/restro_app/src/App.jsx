e1f2795818427de4964c63a624341f353ee44584
Govind Singh <grvrautela90@gmail.com>
2022-04-07 18:23:48 +0530

    assignment

---
+++ React_Revision/Resturant_app/restro_app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { ResturantDetails } from './RestaurantDetails/RestaurantDetails';
+
+function App() {
+  return (
+    <div className="App">
+      <ResturantDetails/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React_Revision/Resturant_app/restro_app/src/RestaurantDetails/RestaurantDetails.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+
+export const ResturantDetails=()=>{
+    const [details, setDetails] = useState("");
+    console.log(details)
+    useEffect(()=>{
+        getData();
+    },[]);
+    function getData(){
+        fetch('http://localhost:3004/data')
+    .then(response => response.json())
+    .then((jsonData) => {
+    // jsonData is parsed json object received from url
+        setDetails(jsonData);
+
+    })
+    .catch((error) => {
+    // handle your errors here
+    console.error(error)
+  })
+
+    }
+    
+    return (
+        <div>
+          {details.map((detail) => {
+            const list = (
+              <>
+                <ul>
+                  <li>Id: {detail.id}</li>
+                  <li>Name: {detail.name}</li>
+                  {/* <li>Age: {employee.age}</li>
+                  <li>City: {employee.city}</li> */}
+                </ul>
+                <hr />
+              </>
+            );
+            return list;
+          })}
+        </div>
+      );    
+}
